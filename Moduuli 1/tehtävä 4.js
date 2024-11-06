7ef60ed7bd0a0c0c92ecc29f5bcf5dabcfd88584
Ville Vartio <ville.vartio@metropolia.fi>
2024-11-06 13:52:07 +0200

    patch notes: added task 1.4

---
+++ Moduuli 1/tehtävä 4.js
@@ -0,0 +1,25 @@
+'use strict';
+
+let number = Math.floor((Math.random() * 4) + 1);
+
+let house = ""
+
+switch (number) {
+    case 1:
+        house = "Griffyndor"
+        break;
+    case 2:
+        house = "Slytherin"
+        break;
+    case 3:
+        house = "Ravenclaw"
+        break;
+    case 4:
+        house = "Hufflepuff"
+        break;
+}
+
+
+const name = prompt("what is your name?: ");
+document.querySelector('#target').innerHTML = 'Congratulations, ' + name +
+    '! ' + 'you are a ' + house;
