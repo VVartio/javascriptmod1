a9214909131f4273a58c9c9038aa2ba7ebfa0c20
Ville Vartio <ville.vartio@metropolia.fi>
2024-11-06 14:56:55 +0200

    patch notes: task 1.10 added

---
+++ Moduuli 1/tehtävä 10.js
@@ -0,0 +1,22 @@
+'use strict';
+const dice = prompt("what is your amount of dice?: ");
+const eyes = prompt("what number do you really want?: ");
+let success = 0
+let what = 100000
+for (let what = 0; what < 100000; what++) {
+    var sum = 0;
+
+    for (var i = 1; i <= dice; i++) {
+        sum += Math.floor(Math.random() * 6) + 1;
+    }
+    ;
+    if (sum == eyes) {
+        success += 1;
+    }
+    ;
+}
+
+let probability = success / what * 100;
+let n = probability.toFixed(2)
+document.getElementById('target').innerHTML = 'Probability to get sum ' + eyes +
+    ' with ' + dice + ' dice is ' + n + '%';
